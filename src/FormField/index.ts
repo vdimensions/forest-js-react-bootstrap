21f99f0f6f02da49563a18e3229dc9e8ed6969a1
Ivaylo Slavov <ivaylo5ev@gmail.com>
2022-01-20 23:33:15 +0200

    - added common form field types

---
+++ src/FormField/index.ts
@@ -0,0 +1,32 @@
+export type FormValidationRule = {
+    isValid?: boolean;
+    message?: string;
+}
+export type FormConstraintValidationRule<T> = FormValidationRule & {
+    constraint: T
+}
+export type FormRegexValidationRule = FormValidationRule & {
+    expression: string
+}
+export type FormCompareValidationRule = FormValidationRule & {
+    name: string;
+}
+
+export type FormValidationState<T> = {
+    required?: FormValidationRule;
+    minLength?: FormConstraintValidationRule<number>;
+    maxLength?: FormConstraintValidationRule<number>;
+    minValue?: FormConstraintValidationRule<T>;
+    maxValue?: FormConstraintValidationRule<T>;
+    format?: FormRegexValidationRule;
+    compare?: FormCompareValidationRule;
+}
+
+export type FormField<T> = {
+    label : string;
+    name: string;
+    value: T;
+    validation: FormValidationState<T>;
+}
+
+const f: FormValidationState<number> = { };
